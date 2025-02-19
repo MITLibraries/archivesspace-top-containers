13d33d941945bc81ee2404794014a5c2655a972a
Eric Hanson <ehanson@mit.edu>
2025-02-19 17:10:53 -0500

    Update cli.py

--- top_containers/cli.py
+++ top_containers/cli.py
@@ -1,15 +1,14 @@
-import csv
+# ruff: noqa: ERA001, PERF401, T201
 import logging
 import os
 import sys
-from datetime import UTC, datetime, timedelta
+from datetime import timedelta
 from time import perf_counter
 
 import click
 from asnake.client import ASnakeClient  # type: ignore[import-untyped]
 
 from top_containers.models import AsOperations
-from top_containers.records import create_instance, create_top_container
 
 logger = logging.getLogger(__name__)
 
@@ -21,31 +20,12 @@ logger = logging.getLogger(__name__)
     prompt="Select the ArchivesSpace instance to use, either 'dev' or 'prod': ",
     type=click.Choice(["dev", "prod"]),
 )
-@click.option(
-    "--directory",
-    required=True,
-    default="data",
-)
-@click.option(
-    "--repository_id",
-    required=True,
-    default="2",
-)
-@click.option(
-    "--metadata_csv",
-    required=True,
-    prompt="Enter the name to the metadata CSV (e.g. 'metadata.csv'): ",
-)
 @click.option("--modify_data", is_flag=True)
 def main(
     as_instance: str,
-    directory: str,
-    repository_id: str,
-    metadata_csv: str,
     modify_data: bool,  # noqa: FBT001
 ) -> None:
     start_time = perf_counter()
-    current_date = datetime.now(tz=UTC)
     logging.basicConfig(
         format="%(asctime)s %(levelname)s %(name)s.%(funcName)s(): %(message)s",
         level=logging.INFO,
@@ -78,42 +58,41 @@ def main(
     as_ops = AsOperations(
         ASnakeClient(baseurl=as_url, username=as_user, password=as_password)
     )
-    with open(f"{directory}/{metadata_csv}", encoding="utf-8") as input_file, open(
-        f"{directory}/{current_date.strftime('%Y-%m-%d_%H.%M.%S')}.csv",
-        "w",
-        encoding="utf-8",
-    ) as output_file:
-        output_csv = csv.DictWriter(output_file, fieldnames=["uri", "title", "data"])
-        output_csv.writeheader()
-        for metadata in csv.DictReader(input_file):
-            top_container = create_top_container(
-                metadata, current_date.strftime("%Y-%m-%d")
-            )
-            top_container_uri = "Dry run, top container URI not created"
-            if modify_data:
-                top_container_post_response = as_ops.post_new_record(
-                    top_container,
-                    f"repositories/{repository_id}/top_containers",
-                )
-                top_container_uri = top_container_post_response["uri"]
-            output_csv.writerow(
-                {"uri": top_container_uri, "title": "NA", "data": top_container}
-            )
+    endpoint = "repositories/2/archival_objects"
+    archival_object_ids = as_ops.client.get(f"{endpoint}?all_ids=true").json()
+    archival_object_ids.sort()
+    chunks = []
+    chunk_size = 50_000
+    for i in range(0, len(archival_object_ids), chunk_size):
+        chunks.append(archival_object_ids[i : i + chunk_size])
+    for chunk in chunks:
+        print(len(chunk))
+    chunk_number = 4
+    archival_objects_to_update = []
+    for ao_count, identifier in enumerate(chunks[chunk_number]):
+        if ao_count % 2000 == 0:
+            print(ao_count)
+        archival_object_uri = f"{endpoint}/{identifier}"
+        archival_object = as_ops.client.get(archival_object_uri).json()
+        update_archival_object = False
+        for note in archival_object["notes"]:
+            if note.get("type") == "accessrestrict":
+                if not note["publish"]:
+                    update_archival_object = True
+                    note["publish"] = True
+                for subnote in [
+                    subnote
+                    for subnote in note.get("subnotes", [])
+                    if not subnote.get("publish")
+                ]:
+                    update_archival_object = True
+                    subnote["publish"] = True
+        if update_archival_object:
+            archival_objects_to_update.append(archival_object_uri)
+            # as_ops.update_record(f"{endpoint}/{identifier}", archival_object)
+    print(archival_objects_to_update)
+    print(len(archival_objects_to_update))
 
-            accession_record = as_ops.get_record(metadata["accession_uri"])
-            instance = create_instance(metadata["instance_type"], top_container_uri)
-            accession_record["instances"].append(instance)
-            if modify_data:
-                as_ops.update_record(accession_record)
-            else:
-                logger.info("Dry run, record '%s' not updated", accession_record["uri"])
-            output_csv.writerow(
-                {
-                    "uri": accession_record["uri"],
-                    "title": accession_record["title"],
-                    "data": instance,
-                }
-            )
     logger.info(
         "Total time to complete process: %s",
         timedelta(seconds=perf_counter() - start_time),
